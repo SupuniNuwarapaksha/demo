7e7f176d008dc84d5bda170d8e2c22113b785fd4
Nadisha_Digiratina <nadisha.bandara@dialog.com>
2022-01-19 15:38:48 +0530

    First commit

---
+++ src/main/java/com/example/demo/LogController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LogController.class);
+	
+	@GetMapping("/log")
+	public String getName(@RequestParam(name = "name") String name) {
+	
+		LOGGER.info("I was logged : name={}", name);
+		return "Hi " + name;
+		
+	}
+
+}
